2fa48f2fd33fc490d0d4fb66c6661d0ba55435d4
Dominic Giglio <humanshell@gmail.com>
2011-08-10 22:00:05 -0400

    added url and path module constants

--- lib/moovatom.rb
+++ lib/moovatom.rb
@@ -2,6 +2,10 @@ require "moovatom/version"
 
 module MoovAtom
   
+  #-- module constants
+  REQ_URL  = 'moovatom.com'
+  REQ_PATH = '/api/api_request'
+  
   class MoovEngine
     
     def initialize(args={})
